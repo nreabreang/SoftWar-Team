6fe8a3021a038be2c17b87eac511c3282b6601f8
Panthon Kansap <panthon_kansap@cmu.ac.th>
2023-01-06 17:36:17 +0700

    Update create-activity.component.js

--- team-app/frontend/src/components/create-activity.component.js
+++ team-app/frontend/src/components/create-activity.component.js
@@ -4,45 +4,105 @@ export default class CreateActivity extends Component {
   constructor(props) {
     super(props);
 
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeDuration = this.onChangeDuration.bind(this);
+    // this.onChangeUsername = this.onChangeUsername.bind(this);
+    // this.onChangeDescription = this.onChangeDescription.bind(this);
+    // this.onChangeDuration = this.onChangeDuration.bind(this);
+    // this.onChangeDate = this.onChangeDate.bind(this);
+    // this.onSubmit = this.onSubmit.bind(this);
+
+    this.onChangeActName = this.onChangeActName.bind(this);
+    this.onChangeActDescription = this.onChangeActDescription.bind(this);
+    this.onChangeVirtualMoney = this.onChangeVirtualMoney.bind(this);
     this.onChangeDate = this.onChangeDate.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
+    // this.state = {
+    //   username: "",
+    //   description: "",
+    //   duration: "",
+    //   date: new Date(),
+    //   users: [],
+    // };
+
     this.state = {
-      username: "",
-      description: "",
-      duration: "",
+      actName: "",
+      actDescription: "",
+      virtualMoney: [],
       date: new Date(),
       users: [],
     };
   }
 
+  // componentDidMount() {
+  //   this.setState({
+  //     users: ["test user"],
+  //     username: "test user",
+  //   });
+  // }
   componentDidMount() {
     this.setState({
       users: ["test user"],
-      username: "test user",
+      actName: "test activity name",
     });
   }
 
-  onChangeUsername(e) {
+  // onChangeUsername(e) {
+  //   this.setState({
+  //     username: e.target.value,
+  //   });
+  // }
+
+  // onChangeDescription(e) {
+  //   this.setState({
+  //     description: e.target.value,
+  //   });
+  // }
+
+  // onChangeDuration(e) {
+  //   this.setState({
+  //     duration: e.target.value,
+  //   });
+  // }
+
+  // onChangeDate(date) {
+  //   this.setState({
+  //     date: date,
+  //   });
+  // }
+
+  // onSubmit(e) {
+  //   e.preventDefault();
+
+  //   const exercise = {
+  //     username: this.state.username,
+  //     description: this.state.description,
+  //     duration: this.state.duration,
+  //     date: this.state.date,
+  //   };
+
+  //   console.log(exercise);
+
+  //   window.location = "/"; //relocation to homepage
+  // }
+
+  onChangeActName(e) {
     this.setState({
-      username: e.target.value,
+      actName: e.target.value,
     });
   }
 
-  onChangeDescription(e) {
+  onChangeActDescription(e) {
     this.setState({
-      description: e.target.value,
+      actDescription: e.target.value,
     });
   }
 
-  onChangeDuration(e) {
+  onChangeVirtualMoney(e) {
     this.setState({
-      duration: e.target.value,
+      virtualMoney: e.target.value,
     });
   }
+
 
   onChangeDate(date) {
     this.setState({
@@ -53,61 +113,62 @@ export default class CreateActivity extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-    const exercise = {
-      username: this.state.username,
-      description: this.state.description,
-      duration: this.state.duration,
+    const activity = {
+      actName: this.state.actName,
+      actDescription: this.state.actDescription,
+      virtualMoney: this.state.virtualMoney,
       date: this.state.date,
     };
 
-    console.log(exercise);
+    console.log(activity);
 
     window.location = "/"; //relocation to homepage
   }
 
   render() {
-    return <div>
-      <form class="w-full max-w-sm" onSubmit={this.onSubmit}>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        Username
-      </label>
-    </div>
-    <select ref="UserInput" 
-    ></select>
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe"/>
+    return (
+      <div>
+        <form class="w-full max-w-sm" onSubmit={this.onSubmit}>
+          <div>
+            <div className="mx-8">
+              <label>Activity Name</label>
+              <input
+                type="text"
+                id="actName"
+                name="actName"
+                className="m-4 border-2 border-black"
+              />
+            </div>
+            <div className="mx-8 my-4 flex items-stretch">
+              <label className="self-center">Description</label>
+              <textarea
+                id="actDes"
+                name="actDes"
+                className="m-4 border-2 border-black"
+              />
+            </div>
+            <div className="mx-8">
+              <label for="virtualMoney">Virtual Money:</label>
+              <select id="virtualMoney" name="virtualMoney" className="m-4">
+                <option value="dolls">dolls</option>
+                <option value="gold chocolate">gold chocolate</option>
+              </select>
+            </div>
+            <div className="mx-8">
+              <label>Date : </label>
+              <input
+                type="date"
+                id="actDate"
+                name="actDate"
+                className="m-4 border-2 border-black"
+              />
+            </div>
+            <div className="mx-8">
+              <input type="submit" value="Submit" className="p-4 rounded-xl px-8"/>
+            </div>
+          </div>
+        </form>
       </div>
-  </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-        Password
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************" />
-    </div>
-  </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3"></div>
-    <label class="md:w-2/3 block text-gray-500 font-bold">
-      <input class="mr-2 leading-tight" type="checkbox"/>
-      <span class="text-sm">
-        Send me your newsletter!
-      </span>
-    </label>
-  </div>
-  <div class="md:flex md:items-center">
-    <div class="md:w-1/3"></div>
-    <div class="md:w-2/3">
-      <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-        Sign Up
-      </button>
-    </div>
-  </div>
-</form>
-    </div>;
+    );
   }
 }
