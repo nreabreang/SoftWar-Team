b66e22594290d5a18517d3c23d6f1b0b1b26c158
Panthon Kansap <panthon_kansap@cmu.ac.th>
2023-01-14 13:35:54 +0700

    Update App.js

--- team-app/frontend/src/App.js
+++ team-app/frontend/src/App.js
@@ -1,17 +1,22 @@
-import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
-import './App.css';
-import Navbar from './components/navbar.component'
-import Homepage from './components/homepage.component';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar.component";
+import Homepage from "./components/homepage.component";
+import CreateActivity from "./components/create-activity.component";
+import ActivityList from "./components/activity-list.component";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <Homepage />
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/createActivity" element={<CreateActivity />}></Route>
+          <Route path="/activityList" element={<ActivityList />}></Route>
+        </Routes>
       </div>
     </Router>
-
   );
 }
 
