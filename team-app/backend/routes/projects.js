119ad5f100ff601ed7e6db785a54fb8351e7012a
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-17 16:07:58 +0700

    update rout project

--- team-app/backend/routes/projects.js
+++ team-app/backend/routes/projects.js
@@ -1,11 +1,31 @@
 const router = require('express').Router();
 let Projects = require('../models/project.model');
+let Commenty = require('../models/comment.model');
 
-router.route().get((req,resp)=>{
+router.route("/").get((req,resp)=>{
     Projects.find()
     .then((Projects)=>resp.status(200).json(Projects))
     .catch((error)=>resp.status(400).json("Error: " + error))
 })
 
+router.route("/add").get((req,resp)=>{
+    const projName = req.body.projectname;
+    const descript = req.body.description;
+    const comment = new Commenty({
 
+    });
+    const timing = Date.parse(req.body.date);
+
+    const newProject = new Projects({
+        projName,
+        descript,
+        comment,
+        timing,
+    });
+
+    newProject
+    .save()
+    .then(()=>resp.json("Add Project is successed."))
+    .catch((error)=>res.status(400).json("Error: " + error))
+})
 
