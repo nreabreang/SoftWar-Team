9c9ac34fe74fe6da10c7ea6ad834f5f0b164dac8
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-19 11:13:48 +0700

    add register and login (not successed)

--- team-app/backend/routes/users.js
+++ team-app/backend/routes/users.js
@@ -1,6 +1,8 @@
 const router = require('express').Router(); //create route
 let User = require('../models/user.model'); // call mongoose models
 
+const SECRET = ""
+
 // first route & handle HTTP Get & Get request
 router.route("/").get((req, res) => {
   // "/users/"
@@ -18,6 +20,41 @@ router.route("/add").post((req, res) => {
     .save() //already save
     .then(() => res.json("User added!"))
     .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/register").post((req,resp)=>{
+  const username = req.body.username
+  const password = req.body.password
+  const email = req.body.email
+
+  const findUsernameInTable = User.find(username)
+  const findPasswordInTable = User.find(password)
+  const findEmailInTable = User.find(email)
+  //check matching in tables
+  if(findUsernameInTable == null || findPasswordInTable == null || findEmailInTable == null){
+    resp.status(400).json("")
+  }else{
+    const newUser = new User({
+    username,
+    password,
+    email,
+  })
+
+  newUser
+  .save()
+  .then(()=>resp.json("Register is successed.")).catch((err)=>{
+    resp.status("400").json("Error: " + err)
+  })
+  }
+});
+
+router.route("/log-in").get((req,resp)=>{
+  const usernameLogIn = req.body.username
+  const passwordLogIn = req.body.password
+  //check matching in table
+  if(true){
+
+  }
 });
 
 module.exports = router;
