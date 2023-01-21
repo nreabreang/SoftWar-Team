4a985da19dcc6e66e3e951bf56db843f6b8e2ca7
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-21 14:53:49 +0700

    create model for guest

---
+++ team-app/backend/models/guest.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const schema = mongoose.Schema;
+
+const guestSchema = new schema({
+    username:{
+        type:String,
+        require:true,
+        unique:true,
+        trim:false,
+        minlength:5
+    },
+},{
+    timestamps:true
+});
+
+const Guest = mongoose.model("Guest",guestSchema)
+
+module.exports = Guest
