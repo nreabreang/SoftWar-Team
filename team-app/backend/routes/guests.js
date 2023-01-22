0e0823b8d98e633913da91620e2521133a85b47f
Panthon Kansap <panthon_kansap@cmu.ac.th>
2023-01-22 18:01:55 +0700

    update guest path

--- team-app/backend/routes/guests.js
+++ team-app/backend/routes/guests.js
@@ -1,8 +1,24 @@
 const router = require('express').Router()
 let Guest = require('../models/guest.model')
 
+// /guest
 router.route("/").get(async(req,resp)=>{
     Guest.find()
     .then((guest)=>resp.json(guest))
     .catch((err)=>resp.status(400).json("Error: " + err))
 })
+
+// /guest/add
+router.route("/add").post((req, res) => {
+    // /activity/add
+    const guestName = req.body.guestName;
+    
+    const newGuest = new Guest({
+      guestName,
+    });
+
+    newGuest
+    .save()
+    .then(() => res.status(200).json("Guest added!"))
+    .catch((err) => res.status(400).json("Error : " + err));
+});
