2a37828c35f4a691e39a885a0a9ae2e7cb8a6d6a
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-24 16:07:15 +0700

    del p

--- team-app/backend/routes/guests.js
+++ team-app/backend/routes/guests.js
@@ -2,25 +2,25 @@ const router = require('express').Router()
 let Guest = require('../models/guest.model')
 
 // /guest
-router.route("/").get(async(req,resp)=>{
+router.route("/").get(async(req,res)=>{
     Guest.find()
-    .then((guest)=>resp.json(guest))
-    .catch((err)=>resp.status(400).json("Error: " + err))
+    .then((guest)=>res.json(guest))
+    .catch((err)=>res.status(400).json("Error: " + err))
 });
 
-router.route("/add").post(async(req,resp)=>{
+router.route("/add").post(async(req,res)=>{
     const guestName = req.body.guestName
     const newGuest = new Guest({guestName})
 
     newGuest.save()
-    .then(()=>resp.status(200).json("Guest add successe."))
-    .catch((err)=>resp.status(400).json("Error: "+err))
+    .then(()=>res.status(200).json("Guest add successe."))
+    .catch((err)=>res.status(400).json("Error: "+err))
 });
 
-router.route("/:id/delete").delete(async(req,resp)=>{
+router.route("/:id/delete").delete(async(req,res)=>{
     Guest.findByIdAndDelete(req.params.id)
-    .then(()=>resp.status(200).json("Guest is deleted successeful."))
-    .catch((err)=>resp.status(400).json("Error: " + err))
+    .then(()=>res.status(200).json("Guest is deleted successeful."))
+    .catch((err)=>res.status(400).json("Error: " + err))
 });
 
 module.exports = router;
