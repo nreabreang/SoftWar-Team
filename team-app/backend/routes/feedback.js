0edfa5da0129ca7d2390a3b677726677d0735052
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-24 12:53:07 +0700

    calculate virtualmoney

--- team-app/backend/routes/feedback.js
+++ team-app/backend/routes/feedback.js
@@ -5,19 +5,23 @@ let Feedback = require('../models/feedback.model');
 // https://localhost:5000/feedback/id/add
 router.route("/:id/add").post(async(req,resp)=>{
     const virtualMoney = req.body.virtualmoney
-    const commentFeedback = req.body.comments
-    const getVirtualMoney = Feedback.find(req.params.id).virtualmoney  //It find by id or Project name to get value
-    const getComments = Feedback.find(req.params.id).comments
-    if(virtualMoney == getVirtualMoney){  //if it same value,it will not change data. 
-
+    const virtualMoneyOld = Feedback.findById(req.params.id)
+    const commentAdd = req.body.comments
+    console.log("Virtualmoney from add: "+virtualMoney)
+    console.log("Virtualmoney from old: "+virtualMoneyOld)
+//    const commentFeedback = req.body.comments
+    Feedback.find
+    if(virtualMoney >= 0){  //if it same value,it will not change data. 
+        const newVirtualMoney = virtualMoney + virtualMoneyOld;
+        console.log(newVirtualMoney)
+        Feedback.findByIdAndUpdate(req.params.id,newVirtualMoney);
     }else{
-
+        resp.status(400).json("Invalid!")
     }
-
-    if(commentFeedback == getComments){
+    if(commentAdd.i_like&&commentAdd.i_wish&&commentAdd.question&&commentAdd.ideas){
 
     }else{
-
+        
     }
 });
 
