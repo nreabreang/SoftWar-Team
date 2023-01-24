6372d036d543bbbc6dc1cbe5ad4a42aeb2022045
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-24 23:43:53 +0700

    add comment

--- team-app/backend/routes/exercises.js
+++ team-app/backend/routes/exercises.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 let Exercise = require("../models/exercise.model");
 
+// http://localhost:5000/exercises/ get all
 router.route("/").get((req, res) => {
   // /exercises/
   Exercise.find() // mongoose command
@@ -8,6 +9,7 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+// http://localhost:5000/exercises/add up to database
 router.route("/add").post((req, res) => {
   // /exercises/add
   const username = req.body.username;
@@ -28,7 +30,7 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error : " + err));
 });
 
-// get data by id
+// http://localhost:5000/exercises/:id get by id
 router.route("/:id").get((req, res) => {
   Exercise.findById(req.params.id)
     .then((exercises) => res.json(exercises))
@@ -36,6 +38,7 @@ router.route("/:id").get((req, res) => {
 });
 
 // delete by id
+// http://localhost:5000/exercises/:id 
 router.route("/:id").delete((req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
     .then((exercises) => res.json("Exercise deleted."))
@@ -43,6 +46,7 @@ router.route("/:id").delete((req, res) => {
 });
 
 //Update by id
+// http://localhost:5000/exercises/update/:id
 router.route("/update/:id").post((req, res) => {
   Exercise.findById(req.params.id)
     .then((exercises) => {
