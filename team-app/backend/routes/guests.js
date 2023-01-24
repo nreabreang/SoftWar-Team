c6abbb5c792aff544a970a5b4a9e8406a55c60f8
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-24 12:12:56 +0700

    add route add,delete

--- team-app/backend/routes/guests.js
+++ team-app/backend/routes/guests.js
@@ -6,4 +6,21 @@ router.route("/").get(async(req,resp)=>{
     Guest.find()
     .then((guest)=>resp.json(guest))
     .catch((err)=>resp.status(400).json("Error: " + err))
-})
+});
+
+router.route("/add").post(async(req,resp)=>{
+    const guestName = req.body.guestName
+    const newGuest = new Guest({guestName})
+
+    newGuest.save()
+    .then(()=>resp.status(200).json("Guest add successe."))
+    .catch((err)=>resp.status(400).json("Error: "+err))
+});
+
+router.route("/:id/delete").delete(async(req,resp)=>{
+    Guest.findByIdAndDelete(req.params.id)
+    .then(()=>resp.status(200).json("Guest is deleted successeful."))
+    .catch((err)=>resp.status(400).json("Error: " + err))
+});
+
+module.exports = router;
