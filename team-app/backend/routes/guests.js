6a2b4c3d56269f3f703e58152cf5d64163e51be0
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-24 17:07:31 +0700

    push

--- team-app/backend/routes/guests.js
+++ team-app/backend/routes/guests.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 let Guest = require('../models/guest.model')
 
-// /guest
+// http://localhost:5000/guest/
 router.route("/").get(async(req,res)=>{
     Guest.find()
     .then((guest)=>res.json(guest))
     .catch((err)=>res.status(400).json("Error: " + err))
 });
-
+// http://localhost:5000/guest/add
 router.route("/add").post(async(req,res)=>{
     const guestName = req.body.guestName
     const newGuest = new Guest({guestName})
@@ -16,8 +16,8 @@ router.route("/add").post(async(req,res)=>{
     .then(()=>res.status(200).json("Guest add successe."))
     .catch((err)=>res.status(400).json("Error: "+err))
 });
-
-router.route("/:id/delete").delete(async(req,res)=>{
+// http://localhost:5000/guest/
+router.route("/delete/:id").delete(async(req,res)=>{
     Guest.findByIdAndDelete(req.params.id)
     .then(()=>res.status(200).json("Guest is deleted successeful."))
     .catch((err)=>res.status(400).json("Error: " + err))
