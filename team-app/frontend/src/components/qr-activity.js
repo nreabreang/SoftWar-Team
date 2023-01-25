84beb9795005d17766eb39407b03d287ec8543d7
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-25 16:47:01 +0700

    del Download

--- team-app/frontend/src/components/qr-activity.js
+++ team-app/frontend/src/components/qr-activity.js
@@ -2,13 +2,11 @@ import QRCode from 'react-qr-code'
 
 export default function GenerateQR(props){
     const urls = props.urls
-    console.log(urls)
     return(
         <div className="">
             <QRCode 
                 value={urls}
             />
-            <a href={urls} download="qrcode.png">Download</a>
         </div>
     );
 }
