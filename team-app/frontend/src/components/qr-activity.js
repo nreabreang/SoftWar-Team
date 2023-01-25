058ad7d9113e860625c655d9836a5bc7c2d81cc5
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-25 13:49:30 +0700

    update function GenerateQR

--- team-app/frontend/src/components/qr-activity.js
+++ team-app/frontend/src/components/qr-activity.js
@@ -1,20 +1,14 @@
-import axios from 'axios'
+import QRCode from 'react-qr-code'
 
 export default function GenerateQR(props){
-    const getUrls = props
-    const pixels = 500
-    axios.get(`https://api.qrserver.com/v1/create-qr-code/?data=${getUrls}&size=${pixels}x${pixels}`)
-    var urls = `https://api.qrserver.com/v1/create-qr-code/?data=${getUrls}&size=${pixels}x${pixels}`
-
+    const urls = props
+    
     return(
         <div classname="">
-            <img src={urls}  />
-            
+            <QRCode 
+                values={urls}
+            />
+            <a href={urls} download='qrcode.png'>Download</a>
         </div>
     );
-}
-
-export default function ReadingQR(props){
-    const getUrls = props.url
-    axios.get(`http(s)://api.qrserver.com/v1/read-qr-code/?fileurl=${getUrls}`)
 }
