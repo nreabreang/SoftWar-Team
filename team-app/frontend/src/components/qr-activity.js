723df77ab1e93338ad366352d4376399db0c96e5
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-26 12:39:37 +0700

    add download

--- team-app/frontend/src/components/qr-activity.js
+++ team-app/frontend/src/components/qr-activity.js
@@ -1,24 +1,19 @@
-import QRCode from 'react-qr-code'
 import {saveAs} from 'file-saver'
 
 export default function GenerateQR(props){
     const urls = props.urls
     const actName = props.actName
+    const openAPI = `http://api.qrserver.com/v1/create-qr-code/?data=${urls}&size=256x256`
 
     const downloadQR = ()=>{
-        saveAs(urls,'qrcode.png')
+        saveAs(openAPI,'qrcode.png')
     }
     return(
         <div className="">
             <label >Scane or Code</label>
-            <QRCode 
-                value={urls}
-                size={64}
-                //style={{width:"",height:"",maxWidth:"",maxHeight:""}} //other choice
-                //level=''  option => 'L'=Low , 'M'=mediam , 'Q'=Quality, 'H'=height
-            />
-            <button onClick={downloadQR()} >Download</button>
-            <p>{actName}</p>
+            <img src={openAPI} width="100" height="100" />
+            <button onClick={downloadQR} >Download</button>
+            {/* <p>{actName}</p> */}
         </div>
     );
 }
