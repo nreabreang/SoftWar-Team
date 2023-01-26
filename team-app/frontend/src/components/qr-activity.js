dfe0690f2398e3f4395a04728f5c8b1fea42f123
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-26 12:26:51 +0700

    add download button

--- team-app/frontend/src/components/qr-activity.js
+++ team-app/frontend/src/components/qr-activity.js
@@ -1,8 +1,13 @@
 import QRCode from 'react-qr-code'
+import {saveAs} from 'file-saver'
 
 export default function GenerateQR(props){
     const urls = props.urls
     const actName = props.actName
+
+    const downloadQR = ()=>{
+        saveAs(urls,'qrcode.png')
+    }
     return(
         <div className="">
             <label >Scane or Code</label>
@@ -12,7 +17,7 @@ export default function GenerateQR(props){
                 //style={{width:"",height:"",maxWidth:"",maxHeight:""}} //other choice
                 //level=''  option => 'L'=Low , 'M'=mediam , 'Q'=Quality, 'H'=height
             />
-            <a href={urls} download="qrcode.png">Download</a>
+            <button onClick={downloadQR()} >Download</button>
             <p>{actName}</p>
         </div>
     );
