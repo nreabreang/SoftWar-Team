5d38061f1bece63ec3947de81aeb262b3b17c6f7
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-01-29 16:49:48 +0700

    adjust function and class

--- team-app/frontend/src/components/guest-view/guest-enter.js
+++ team-app/frontend/src/components/guest-view/guest-enter.js
@@ -1,7 +1,33 @@
 import { Component } from "react";
 import Scanner from "../scanner.component";
 
+const deCode = (code)=>{
+    var decodeTeext = ""
+    return decodeTeext
+}
+
 export default class guestEnter extends Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+            input:""
+        }
+
+        this.changeInputText = this.changeInputText.bind(this)
+    }
+
+    changeInputText(e){
+        this.setState({
+            input:e
+        })
+        deCode(this.state.input)
+    }
+
+    changePage(){
+        window.location
+    }
+
     render(){
         return(
             <div>
@@ -9,7 +35,11 @@ export default class guestEnter extends Component{
                 <div className="flex justify-center mt-8">
                 <div className="flex p-8 bg-white ">
           <Scanner width={256} height={256} />
-        </div>
+                </div>
+                </div>
+                <div className="">
+                    <input type="text" onChange={(e)=>this.changeInputText(e.currentTarget.value)}/>
+                    <button onSubmit={this.changePage()}>Submit</button>
                 </div>
             </div>
         );
