7b4d079f1017fb197df8dd74e272b57a6ce982d4
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-02-03 13:18:07 +0700

    del comments

--- team-app/frontend/src/components/projects-list.component.js
+++ team-app/frontend/src/components/projects-list.component.js
@@ -1,49 +1,50 @@
 import axios from "axios";
 import { Component } from "react";
 
-const Projects = (props) =>{
+const Project = (props) =>{
     return(
-        <div className="">
-            <h1 className=" text-[20px]">{this.props.projectName}</h1>
-            <p className="text-[16px]">{this.props.projectDescription}</p>
-            <p className="text-[16px]">{this.props.projectComments}</p>
+        <div className=" bg-white">
+            <h1 className=" text-[20px]">{props.projectName}</h1>
+            <p className="text-[16px]">{props.projectDescription}</p>
         </div>
     )
 }
 
-export default class projectList extends Component{
+export default class ProjectLists extends Component{
     constructor(props){
-        super(props);
-        this.setProjectList = this.setProjectList.bind(this)
+        super(props)
+
+        this.deleteProject = this.deleteProject.bind(this)
         this.state={
             projects:[],
         }
     };
-    
+
     componentDidMount(){
-        axios.get('http://localhost:5000/project/').then((res)=>this.setState({
-            projects:res.data
-        })).catch((err)=>console.log(err))
-    };
+        axios.get("http://localhost:5000/project/")
+        .then((res)=>{
+            this.setState({projects: res.data})
+            console.log(this.state.projects)
+        })
+        .catch((err)=>console.log(err))
+    };  
 
 
     deleteProject(id){
         axios.post('http://localhost:5000/project/delete/'+id)
-
         window.location = "/project/"
         this.setState({
-            projects:this.state.projects.filter((val)=>val.id != id)
+            projects:this.state.projects.filter((val)=>val.id !== id)
         })
     };
 
     showProjectList(){
-        return this.state.projects.map((resdata)=>{
+        return this.state.projects.map((resdata,index)=>{
             return(
                 <div className="bg-sky-500">
-                    <Projects
+                    <Project
                     projectName={resdata.projectName}
                     projectDescription={resdata.description}
-                    projectComments={resdata.comments}
                     />
                 </div>
             )
