654d9621a66769c8009d7cfe889c8909c8ddb958
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-02-05 22:16:26 +0700

    add input comments

--- team-app/frontend/src/components/project-id.component.js
+++ team-app/frontend/src/components/project-id.component.js
@@ -6,7 +6,8 @@ export default class projectID extends Component{
         super(props)
         this.state={
             projectName:"",
-            description:""
+            description:"",
+            comensts:[]
         }
     }
 
@@ -23,12 +24,50 @@ export default class projectID extends Component{
         .catch((err)=>console.log("Error: "+err))
     }
 
+    virtualMoneyOnChange(number){
+        console.log(number)
+    }
+
+    commentILikeOnChange(texts){
+        console.log(texts)
+    }
+
+    commentIWishOnChange(texts){
+        console.log(texts)
+    }
+
+    commentQuestionOnChange(texts){
+        console.log(texts)
+    }
+
+    commentIdeaOnChange(texts){
+        console.log(texts)
+    }
+
+    onSubmitAction(){
+        axios.post().then().catch()
+    }
+
     render(){
         return(
-            <div className="m-2 bg-white flex  justify-center">
+            <div className="m-2 bg-white flex  flex-col">
                 <div>{this.state.projectName}</div>
                 <div dangerouslySetInnerHTML={{__html:"Description: " + this.state.description}}></div>
                 <div>Comment</div>
+                <form action="" onSubmit={this.onSubmitAction}>
+                    <div className="">
+                    <p>Virtual Money</p>
+                    <input type="number" onChange={(e)=>this.virtualMoneyOnChange(e.target.value)}/>
+                </div>
+                <div className="">
+                    <input type="text" placeholder="I like ..." id="ILike" name="ILike" onChange={(e)=>this.commentILikeOnChange(e.target.value)}/>
+                    <input type="text" placeholder="I wish ..." id="IWish" name="IWish" onChange={(e)=>this.commentIWishOnChange(e.target.value)}/>
+                    <input type="text" placeholder="Question?" id="Question" name="Question" onChange={(e)=>this.commentQuestionOnChange(e.target.value)}/>
+                    <input type="text" placeholder="Idea?" id="Ideas" name="Ideas" onChange={(e)=>this.commentIdeaOnChange(e.target.value)}/>
+                </div>
+                <input type="submit" value="Submit"/>
+                </form>
+                
             </div>
         )
     }
