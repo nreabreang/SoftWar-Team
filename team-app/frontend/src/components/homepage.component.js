32d2df5e65e1b22e60d3f599920e7a1f74b509f5
limthelimn <nuttawan.lim@gmail.com>
2023-02-08 20:59:22 +0700

    frontend update

--- team-app/frontend/src/components/homepage.component.js
+++ team-app/frontend/src/components/homepage.component.js
@@ -4,7 +4,7 @@ import "./homepage.component.css";
 import rightarrow from "./images/right-arrow.png";
 import qr from "./images/qr-code - 1.png";
 import "./homepage.component.css";
-import "./Styles.css";
+import "./styles.css";
 import axios from "axios";
 import { Buffer } from "buffer";
 
