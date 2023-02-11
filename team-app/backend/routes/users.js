4035a15363223d6ed3885d90c242c2cf54073b91
zen138yanatib <zen2002yanatib@gmail.com>
2023-02-11 22:43:30 +0700

    Update users.js

--- team-app/backend/routes/users.js
+++ team-app/backend/routes/users.js
@@ -61,4 +61,28 @@ router.route("/log-in").get(async (req,resp)=>{
   }
 });
 
+router.route("/google-sign-in").post((req,resp)=>{
+   // Get the Google profile data (basic)
+    //var profile = googleUser.getBasicProfile();
+    
+    // Retrieve the Google account data
+    gapi.client.load('oauth2', 'v2', function () {
+      var request = gapi.client.oauth2.userinfo.get({
+          'userId': 'me'
+      });
+      request.execute(function (resp) {
+          // Display the user details
+          var profileHTML = '<h3>Welcome '+resp.given_name+'! <a href="javascript:void(0);" onclick="signOut();">Sign out</a></h3>';
+          profileHTML += '<img src="'+resp.picture+'"/><p><b>Google ID: </b>'+resp.id+'</p><p><b>Name: </b>'+resp.name+'</p><p><b>Email: </b>'+resp.email+'</p><p><b>Gender: </b>'+resp.gender+'</p><p><b>Locale: </b>'+resp.locale+'</p><p><b>Google Profile:</b> <a target="_blank" href="'+resp.link+'">click to view profile</a></p>';
+          document.getElementsByClassName("userContent")[0].innerHTML = profileHTML;
+          
+          document.getElementById("gSignIn").style.display = "none";
+          document.getElementsByClassName("userContent")[0].style.display = "block";
+          
+          // Save user data
+          saveUserData(resp);
+      });
+    });
+});
+
 module.exports = router;
