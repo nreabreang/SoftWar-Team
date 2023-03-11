336740f90e6b9a8391a929c30b00253935e0f7c9
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-03-11 23:04:32 +0700

    Delete bug

--- team-app/frontend/src/components/creator-view/project-list.component.js
+++ team-app/frontend/src/components/creator-view/project-list.component.js
@@ -44,64 +44,8 @@ const Project = (props) => {
                     </div>
                 </Link>
             </div>
-
-
-        </div>
-        // <div className="list-container mx-auto
-        //                 xs:w-80
-        //                 sm:w-80
-        //                 md:w-72
-        //                 lg:w-100
-        //                 xl:w-80">
-
-        //     {/* header */}
-        //     <div className="list-header-container text-24px bold">
-        //         <div className="flex ellipsis">
-        //             {props.projectName}
-        //         </div>
-        //     </div>
-
-        //     {/* description */}
-        //     <div className="mt-2">
-
-        //         {/* description head */}
-        //         <div className="items-container">
-        //             <p className="text-16px bold">DESCRIPTION</p>
-        //         </div>
-
-        //         <div className="line" />
-
-        //         {/* description */}
-        //         <div className="items-container">
-        //             {/* <p className="text-16px italic ellipsis">{props.projectDescription}</p> */}
-        //             <div className="text-16px ellipsis break-words" dangerouslySetInnerHTML={{ __html: des }}></div>
-        //         </div>
-
-        //         {/* see project */}
-
-        //         <Link to={"/creatorprojectList/" + props.projectID}>
-        //             <div className="enter-container">
-        //                 <p className="text-14px underline italic">MORE</p>
-        //                 <img src={rightarrow} className="images-16px" />
-        //             </div>
-        //         </Link>
-
-        //     </div>
-        // </div >
-
-        /* <div className="overflow-hidden flex flex-col bg-white p-3 m-4 shadow sm:rounded-lg" >
-            <h1 className=" text-20px">Project Name: {props.projectName}</h1>
-            <div className="border-t border-gray-200"></div>
-            <p className="text-16px">ข้อมูล: {props.projectDescription}</p>
-            <div className="border-t border-gray-200">
-                <dl>
-                    <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 ">
-                        <dt className="text-sm font-medium text-gray-500">Description</dt>
-                        <div className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0" dangerouslySetInnerHTML={{ __html: des }}></div>
-                    </div></dl></div>
-            <Link to={"/creatorprojectList/" + props.projectID} >See</Link>
-        </div> */
-
+         </div >
+    </div>
     )
 }
 
