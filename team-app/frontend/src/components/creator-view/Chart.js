c2ce956ed79588492ce1587afc2848e9ed682c7d
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-03-13 13:01:21 +0700

    ลบส่วนที่ไม่ได้ใช้งาน

--- team-app/frontend/src/components/creator-view/Chart.js
+++ team-app/frontend/src/components/creator-view/Chart.js
@@ -1,15 +1,12 @@
-import React, { Component, useRef } from "react";
+import React, { useRef } from "react";
 import Chart from "chart.js/auto";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 
 
 
 const BarChart = ({ data, options }) => {
   const canvasRef = useRef(null);
-  const [chartData, setChartData] = useState([]);
-
-  
 
   useEffect(() => {
     const chartInstance = new Chart(canvasRef.current, {
